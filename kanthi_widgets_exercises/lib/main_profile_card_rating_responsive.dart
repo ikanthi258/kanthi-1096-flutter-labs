8f97be893f99b80a19d3b55beaa3a964f57a7caa
ikanthi258 <kanthi.p@kkumail.com>
2026-01-15 18:31:44 +0700

    Lab6 exercise1

---
+++ kanthi_widgets_exercises/lib/main_profile_card_rating_responsive.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'components/contact_image.dart';
+import 'components/contact_info.dart';
+import 'components/ratings.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile Card',
+      // Dark Theme Configuration
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.orange,
+          brightness: Brightness.dark,
+        ),
+      ),
+      themeMode: ThemeMode.system,
+      home: MainProfileCardRatingResponsive(),
+    ),
+  );
+}
+
+class MainProfileCardRatingResponsive extends StatelessWidget {
+  const MainProfileCardRatingResponsive({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    if (orientation == Orientation.portrait) {
+      return _buildPortraitLayout();
+    } else {
+      return _buildLandscapeLayout();
+    }
+  }
+}
+
+Scaffold _buildPortraitLayout() {
+  return Scaffold(
+    body: Container(
+      decoration: BoxDecoration(color: Colors.cyanAccent),
+      padding: EdgeInsets.all(10),
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ContactImage(imagePath: 'assets/ck_cheong.jpg', name: 'CK Cheong'),
+            ContactInfo(
+              addressName: 'Fastwork Technologies HQ',
+              addressInfo:
+                  'Emporium Tower 622 Floor 24,Sukhumvit Rd, Khlong Toei, Bangkok 10110',
+              email: 'ck@fastwork.co',
+              phone: '(012) 345 6789',
+            ),
+            Ratings(defaultColor: Colors.black, ratingColor: Colors.green),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Scaffold _buildLandscapeLayout() {
+  return Scaffold(
+    body: Container(
+      decoration: BoxDecoration(color: Colors.cyanAccent),
+      padding: EdgeInsets.all(10),
+      child: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ContactImage(
+                    imagePath: 'assets/ck_cheong.jpg',
+                    name: 'CK Cheong',
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ContactInfo(
+                    addressName: 'Fastwork Technologies HQ',
+                    addressInfo:
+                        'Emporium Tower 622 Floor 24,Sukhumvit Rd, Khlong Toei, Bangkok 10110',
+                    email: 'ck@fastwork.co',
+                    phone: '(012) 345 6789',
+                  ),
+                  Ratings(
+                    defaultColor: Colors.black,
+                    ratingColor: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
