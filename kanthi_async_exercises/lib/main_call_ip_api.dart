c483f5ad500bbf093289d89d6e138324393452e9
ikanthi258 <kanthi.p@kkumail.com>
2026-02-12 17:36:07 +0700

    lab8 exercise1

--- kanthi_async_exercises/lib/main_call_ip_api.dart
+++ kanthi_async_exercises/lib/main_call_ip_api.dart
@@ -1,3 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+// Fetches the public IP from API and displays it //
+Future<void> fetchIPAddress() async {
+  try {
+    final response = await http.get(
+      Uri.parse('https://api.ipify.org?format=json'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body); // Parse JSON response
+      final ipAddress = data['ip']; // Extract IP address from the parsed data
+
+      print('Your public IP address is: $ipAddress');
+    } else {
+      print('Failed to fetch IP address. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error occurred: $e');
+  }
+}
+
 void main() async {
   await fetchIPAddress();
 }
